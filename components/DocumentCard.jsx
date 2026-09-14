47b9d4f43027efb6c2edf0a7d9e9c759ad465f42
Munkhuu Munkhuu <gmunkherdene.18022@gmail.com>
2026-09-14 21:59:54 +0800

    Update document card details for user information

--- components/DocumentCard.jsx
+++ components/DocumentCard.jsx
@@ -4,9 +4,9 @@ export default function DocumentCard({type='id',onClick}){
     <div className="doc-title">{passport?'PASSPORT • DEMO':'CITIZEN IDENTITY CARD • DEMO'}</div>
     <div className="doc-photo">PHOTO</div>
     <div className="doc-info">
-      <small>Овог / Surname</small><b>Ганбат</b>
-      <small>Нэр / Given name</small><b>-МӨНХ-ЭРДЭНЭ</b>
-      <small>Төрсөн огноо / Date of birth</small><b>2004 / 12 / 24</b>
+      <small>Овог / Surname</small><b>Болорхүү</b>
+      <small>Нэр / Given name</small><b>БАТМЭНД</b>
+      <small>Төрсөн огноо / Date of birth</small><b>2004 / 08 / 20</b>
       <small>Дугаар / No.</small><b>658651499738</b>
     </div>
   </button>
